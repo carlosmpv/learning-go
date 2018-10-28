5ed446f9594e81f95ba1ef6ed0a4195bf134a2b4
no1 <carlosmpvaldes@gmail.com>
2018-10-27 22:59:43 -0300

    Creating first test

--- golang-syntax-tests/syntax-tests.go
+++ golang-syntax-tests/syntax-tests.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+var server http.Server
+var host string
+var port int
+
 func main() {
-	fmt.Println("Wake me up")
+	host = "localhost"
+	port = 8080
+
+	server = http.Server{
+		Addr:    fmt.Sprintf("%s:%d", host, port),
+		Handler: nil,
+	}
+
+	fmt.Printf("Server started at: %s:%d", host, port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func responseHandler(w http.ResponseWriter, r *http.Request) {
+
 }
